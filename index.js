45babcc329d16aa46cfdc93148f3b9f77205def1
Vu Minh Man <vuminhmanmlm@gmail.com>
2022-03-13 11:32:03 +0700

    First commit.

---
+++ index.js
@@ -0,0 +1,14 @@
+const courses = [];
+
+const courseA = { id: 1, name: "A" };
+const courseB = { id: 2, name: "B" };
+const courseC = { id: 3, name: "C" };
+courses.push(courseA);
+courses.push(courseB);
+courses.push(courseC);
+
+function sum(a, b) {
+  return a + b;
+}
+
+export { courses, sum };
